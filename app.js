74d0c5717de6af7746a668b7668f282296d3dbe1
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-05-03 14:31:04 +0200

    up date

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 $(function(){
     var player = '<div id="player"></div>';
-    $('#map').append(player);
+    $('body').append(player);
+    var positionX = 0;
+    var positionY = 0;
     $(document).keydown(function(e){
+        var windowWidth = $(window).width() -300;
+        var windowHeight = $(window).height() -220;
+        
         // alert(e.keyCode);
         var position = $('#player').position();
         //in positions we only have top and left == (x and y)
         // alert(position.top);
         switch(e.keyCode){
-            case 37: // left arrow
-            $('#player').css('left', position.left - 20 + 'px');
+            case 37: // left 
+            positionX = positionX - 20;
+            if (positionX > 0){
+                $('#player').css("margin-left", positionX );
+            }else if (positionX < 0){
+                positionX = windowWidth;
+            };
             break;
-                case 38: // up arrow
-                $('#player').css('top', position.top -20 + 'px');
-                break;
-                    case 39: // right arrow
-                    $('#player').css('left' , position.left +20 + 'px');
-                    break;
-                        case 40: // down arrow
-                        $('#player').css('top' , position.top +20 + 'px');
-                        break;
+            case 38: // up 
+            positionY = positionY - 20; 
+            if(positionY > 0){
+                $('#player').css("margin-top" , positionY);
+            }else if (positionY < 0){
+                positionY = windowHeight;
+            };
+            break;
+
+            case 39: // right 
+            positionX = positionX + 20; 
+            if(positionX < windowWidth){
+                $('#player').css("margin-left" ,  positionX);
+            }else if ( positionX > windowWidth){
+                positionX = 0;
+            };     
+            break;
+            case 40: // down 
+            positionY = positionY + 20;
+            if(positionY < windowHeight){
+                $('#player').css("margin-top" , positionY);
+            }else if(positionY > windowHeight){
+                positionY = 0;
+            };  
+            break;
         };
     });
-
 });
+
+
+
